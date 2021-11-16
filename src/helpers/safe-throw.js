1c1b067e7525e691627cc0118588a6cafb87621f
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-11-16 15:38:21 -0300

    add safe throw helper
    to handle throws inside a express handler (v4 dont handle correct yet)

---
+++ src/helpers/safe-throw.js
@@ -0,0 +1,9 @@
+const safeThrow = (fn) => async (req, res, next) => {
+  try {
+    return await fn(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { safeThrow };
